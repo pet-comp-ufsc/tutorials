116de32af0ace66e21c90eaac0559fcd2f3c06e5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-29 07:12:56 +0000

    Deploy pet-comp-ufsc/tutorials to github.com/pet-comp-ufsc/tutorials.git:gh-pages

---
+++ js/special-tags.js
@@ -0,0 +1,15 @@
+function feedSpecialTags() {
+    let tags = ['concept', 'warn', 'trap'];
+
+    for (let tag of tags) {
+        let specialElms = document.getElementsByTagName(`${tag}`);
+
+        for (let elm of specialElms) {
+            let specialTag = document.createElement(`${tag}tag`);
+            specialTag.innerHTML = `<title>${elm.title}</title>`;
+            elm.parentNode.insertBefore(specialTag, elm);
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', feedSpecialTags);
